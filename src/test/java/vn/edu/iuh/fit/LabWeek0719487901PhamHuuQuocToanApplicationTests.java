688cc424e981ed5ab2d448fbdb708d05b5d386da
quoctoan <quoctoanlk2019@gmail.com>
2023-11-04 07:44:49 +0700

    first commit

---
+++ src/test/java/vn/edu/iuh/fit/LabWeek0719487901PhamHuuQuocToanApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWeek0719487901PhamHuuQuocToanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
